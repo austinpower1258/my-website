4cbec679ac4450439b2a0bff9f128c1f75e5d540
austinpower1258 <austinpower1258@gmail.com>
2021-08-10 13:47:31 -0700

    changed web layout

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,57 +3,46 @@
 </script>
 
 <script lang="ts">
-	import Counter from '$lib/Counter.svelte';
+	
+	import GitHubIcon from '$lib/icons/GitHubIcon.svelte';
+	import TwitterIcon from '$lib/icons/GitHubIcon.svelte';
+	import MailIcon from '$lib/icons/MailIcon.svelte';
+
 </script>
 
 <svelte:head>
 	<title>Home</title>
 </svelte:head>
 
-<section>
-	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
+<main class="max-w-prose mx-auto p-5">
+	<section>
+		<h1>I'm Austin Wang</h1>
+	</section>
+	<section>
+		<h2 class="sr-only">Contact me</h2>
+		<div class="flex space-x-3">
+			<a
+				class="group"
+				href="https://twitter.com/austinpcwang"
+				target="_blank"
+				rel="external noopener noreferrer"
+			>
+				<span class="sr-only">My Twitter</span>
+				<TwitterIcon class="w-5 h-5 text-gray-500 group-hover:text-black transition" />
+			</a>
+			<a
+				class="group"
+				href="https://github.com/austinpower1258"
+				target="_blank"
+				rel="external noopener noreferrer"
+			>
+				<span class="sr-only">My GitHub</span>
+				<GitHubIcon class="w-5 h-5 text-gray-500 group-hover:text-black transition" />
+			</a>
+			<a class="group" href="mailto: austinwang115@gmail.com">
+				<span class="sr-only">My Mail</span>
+				<MailIcon class="w-5 h-5 text-gray-500 group-hover:text-black transition" />
+			</a>
 		</div>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+	</section>
+</main>
